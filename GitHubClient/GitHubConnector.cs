a8d26965eae7e088c4584efe38ea0ffcad1df8fc
guinohenostroza <guinohenostroza@gmail.com>
2022-02-07 00:48:02 -0500

    bad code 4

--- GitHubClient/GitHubConnector.cs
+++ GitHubClient/GitHubConnector.cs
@@ -58,5 +58,37 @@ namespace GitHubClient
             return true;
         }
 
+        private void BugsSamples()
+        {
+            string select = "SELECT p.fname, p.lname, p.street1, p.street2, p.city, p.state, p.zip" +
+                            "FROM person p" +  // Noncompliant; concatenates to: p.zipFROM
+                            "WHERE p.id = @ID";  // Noncompliant; concatenates to: pWHERE
+
+            int target = -5;
+            int num = 3;
+
+            target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+            target =+ num; // Noncompliant; target = 3
+
+            bool a = false;
+            bool b = false;
+
+            if (!a || b) // Noncompliant; "!a" is always "true", "b" is never evaluated
+            {
+               // always executed
+            }
+            else
+            {
+               // never executed
+            }
+
+
+
+
+
+
+
+        }
+
     }
 }
